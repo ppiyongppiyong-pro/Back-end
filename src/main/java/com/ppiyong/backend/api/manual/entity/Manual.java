cc10a1f75e729f382757517291375fe228c30eb8
kite_U <yurry45@naver.com>
2025-04-08 12:30:41 +0900

    feature/#1 : Manual Entity 클래스 생성 및 필드 추가

--- src/main/java/com/ppiyong/backend/api/manual/entity/Manual.java
+++ src/main/java/com/ppiyong/backend/api/manual/entity/Manual.java
@@ -1,4 +1,32 @@
 package com.ppiyong.backend.api.manual.entity;
 
-public class Manual {
+import com.ppiyong.backend.api.common.BaseEntity;
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "manual")
+public class Manual extends BaseEntity {
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "manual_id")
+    private Long id;
+
+    @Column(name = "name", nullable = false)
+    private String name;
+
+    @Column(name = "manual_summary")
+    private String manualSummary;
+
+    @Column(name = "detail")
+    private String detail;
+
+    @Column(name = "category")
+    private String category;
+
+    @Column(name = "keyword")
+    private String keyword;
+
+    @Column(name = "imgurl")
+    private String imgurl;
+
 }
