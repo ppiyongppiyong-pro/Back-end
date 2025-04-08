018f9a3a1149b700711f7c91abfb59ddee2a3476
kite_U <yurry45@naver.com>
2025-04-08 12:30:32 +0900

    feature/#1 : Hospital Entity 클래스 생성 및 필드 추가

---
+++ src/main/java/com/ppiyong/backend/api/emergencymap/entity/Hospital.java
@@ -0,0 +1,40 @@
+package com.ppiyong.backend.api.emergencymap.entity;
+
+import com.ppiyong.backend.api.common.BaseEntity;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Getter;
+
+@Entity
+@Table(name = "hospital")
+public class Hospital extends BaseEntity {
+
+    // 추가된 메서드
+    @Getter
+    @Id
+    @Column(unique = true, nullable = false)
+    private String placeId;
+
+    @Column(name = "place_name")
+    private String placeName;
+
+    @Column(name = "address_name")
+    private String addressName;
+
+    @Column(name = "road_address_name")
+    private String roadAddressName;
+
+    @Column(name = "category_name")
+    private String categoryName;
+
+    @Column(name = "phone")
+    private String phone;
+
+    @Column(name = "point_x")
+    private String x;
+
+    @Column(name = "point_y")
+    private String y;
+}
