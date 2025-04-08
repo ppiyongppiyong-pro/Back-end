a71db79d97de439de3e91145e63dce7e8200d26a
kite_U <yurry45@naver.com>
2025-04-08 12:30:51 +0900

    feature/#1 : Member Entity 클래스 생성 및 필드 추가

--- src/main/java/com/ppiyong/backend/api/member/entity/Member.java
+++ src/main/java/com/ppiyong/backend/api/member/entity/Member.java
@@ -1,4 +1,34 @@
 package com.ppiyong.backend.api.member.entity;
 
-public class Member {
+import com.ppiyong.backend.api.common.BaseEntity;
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "member")
+public class Member extends BaseEntity {
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "member_id", unique = true, nullable = false)
+    private Long id;
+
+    @Column(name = "name", nullable = false)
+    private String name;
+
+    @Column(name = "email", nullable = false)
+    private String email;
+
+    @Column(name = "password")
+    private String password;
+
+    @Column(name = "phone_number")
+    private String phoneNumber;
+
+    @Column(name = "gender")
+    private String gender;
+
+    @Column(name = "parent_phone_number")
+    private String parentPhoneNumber;
+
+    @Column(name = "address")
+    private String address;
 }
