901443fea47df10cf22b3988629437765efc8c27
kite_U <yurry45@naver.com>
2025-04-26 14:03:47 +0900

    feature/#3 : token 관련 ErrorCode 해결

--- src/main/java/com/ppiyong/backend/global/exception/ErrorCode.java
+++ src/main/java/com/ppiyong/backend/global/exception/ErrorCode.java
@@ -11,6 +11,7 @@ public enum ErrorCode {
     MEMBER_ALREADY_EXISTS(404, "ME002", "이미 존재하는 회원입니다."),
     INTERVAL_SERVER_ERROR(500, "SE001", "서버 내부의 오류입니다."),
     TEST_ERROR(404, "TEST", "테스트 에러입니다."),
+
     // Manual
     MANUAL_NOT_FOUND(404, "ME001", "매뉴얼을 찾을 수 없습니다."),
 
@@ -24,6 +25,11 @@ public enum ErrorCode {
     INVALID_KAKAO_ACCESS_TOKEN(401, "TE004", "카카오 토큰이 유효하지 않습니다."),
     EMPTY_TOKEN(401, "TE005", "토큰이 비어있습니다."),
     EMPTY_COOKIE(401, "TE006", "쿠키가 비어있습니다."),
+    FAILED_GENERATE_TOKEN(401, "TE007", "토큰 생성에 실패하였습니다."),
+    FAILED_JWT_INFO(401, "TE006", "토큰으로부터 회원의 정보를 얻을 수 없습니다."),
+    INVALID_JWT_SIGNATURE(401, "TE007", "유효하지 않은 서명의 JWT 토큰입니다."),
+    UNSUPPORTED_JWT_TOKEN(401, "TE008", "지원하지 않는 형식의 JWT 토큰입니다."),
+
     // login
     UNAUTHORIZED(401, "LE001", "인증되지 않았습니다."),
     UNMATCHED_PASSWORD(404, "LE002", "비밀번호가 일치하지 않습니다."),
