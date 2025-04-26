016ddd66f7d263efc2adadbdac0e3f6110f50983
kite_U <yurry45@naver.com>
2025-04-26 12:29:56 +0900

    feature/#4 : 응답 실패에 대한 ErrorResponse 생성

---
+++ src/main/java/com/ppiyong/backend/global/response/ErrorResponse.java
@@ -0,0 +1,25 @@
+package com.ppiyong.backend.global.response;
+
+import com.ppiyong.backend.global.exception.ErrorCode;
+import lombok.Builder;
+import org.springframework.http.ResponseEntity;
+
+@Builder
+public class ErrorResponse {
+
+    private int status;
+    private String code;
+    private String message;
+
+    // ResponseBody 설정
+    public static ResponseEntity<ErrorResponse> toResponseEntity(ErrorCode errorCode) {
+        return ResponseEntity
+                .status(errorCode.getStatus())
+                .body(ErrorResponse.builder()
+                        .status(errorCode.getStatus())
+                        .code(errorCode.getCode())
+                        .message(errorCode.getMessage())
+                        .build()
+                );
+    }
+}
