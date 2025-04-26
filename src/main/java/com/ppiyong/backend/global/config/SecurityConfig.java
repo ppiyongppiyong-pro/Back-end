7eaec048bad6c5986fca59c55c4ce315b90fbf2a
kite_U <yurry45@naver.com>
2025-04-26 14:07:23 +0900

    feature/#3 : SecurityConfig 생성 및 filter, permission 설정 메소드 구현

--- src/main/java/com/ppiyong/backend/global/config/SecurityConfig.java
+++ src/main/java/com/ppiyong/backend/global/config/SecurityConfig.java
@@ -1,11 +1,78 @@
 package com.ppiyong.backend.global.config;
 
+import com.ppiyong.backend.api.member.repository.MemberRepository;
+import com.ppiyong.backend.global.auth.JwtAuthenticationFilter;
+import com.ppiyong.backend.global.auth.TokenProvider;
 import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
-//@Configuration
-//@RequiredArgsConstructor
-//public class SecurityConfig {
-//
-//
-//}
+import java.util.List;
+
+@Configuration
+@RequiredArgsConstructor
+public class SecurityConfig {
+
+    private final MemberRepository memberRepository;
+    private final TokenProvider tokenProvider;
+
+    @Bean
+    public BCryptPasswordEncoder bCryptPasswordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity
+                .cors(httpSecurityCorsConfigurer ->
+                        httpSecurityCorsConfigurer.configurationSource(corsConfigurationSource()))
+                .csrf(AbstractHttpConfigurer::disable)
+                .httpBasic(AbstractHttpConfigurer::disable)
+                .sessionManagement(sessionManagementConfigurer ->
+                        sessionManagementConfigurer.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+        ;
+
+        setTokenFilter(httpSecurity);
+        setPermission(httpSecurity);
+
+        return httpSecurity.build();
+    }
+
+    private void setTokenFilter(HttpSecurity httpSecurity) throws Exception {
+        JwtAuthenticationFilter jwtAuthenticationFilter = new JwtAuthenticationFilter(memberRepository, tokenProvider);
+        httpSecurity.addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
+    }
+
+
+
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration conf = new CorsConfiguration();
+
+        conf.setAllowedOrigins(List.of("http://localhost:3000"));
+        conf.setAllowedMethods(List.of("OPTIONS", "HEAD", "GET", "POST", "PUT", "PATCH", "DELETE"));
+        conf.setAllowCredentials(true);
+        conf.setAllowedHeaders(List.of("Authorization", "Content-Type"));
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", conf);
+
+        return source;
+    }
+
+    private void setPermission(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity.authorizeHttpRequests(auth -> auth
+                .requestMatchers("/**").permitAll()  // 추후에 접근 설정 추가
+                .anyRequest().authenticated()
+        );
+    }
+}
