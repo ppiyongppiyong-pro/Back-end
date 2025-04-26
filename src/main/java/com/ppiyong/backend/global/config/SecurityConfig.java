ec1e9961ba015a180db8bbaf7b3d879959e6cad0
kite_U <yurry45@naver.com>
2025-04-26 12:29:03 +0900

    feature/#4 : SecurityConfig 생성

---
+++ src/main/java/com/ppiyong/backend/global/config/SecurityConfig.java
@@ -0,0 +1,11 @@
+package com.ppiyong.backend.global.config;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@RequiredArgsConstructor
+public class SecurityConfig {
+
+
+}
