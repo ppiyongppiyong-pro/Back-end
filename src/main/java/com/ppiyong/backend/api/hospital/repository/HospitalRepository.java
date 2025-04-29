1ce6b666b19a39213c388b6b253fdb51037b27a9
LHL <lin1137001@gmail.com>
2025-04-30 02:41:18 +0900

    feature/#5 : hospitalId로 병원 조회

--- src/main/java/com/ppiyong/backend/api/hospital/repository/HospitalRepository.java
+++ src/main/java/com/ppiyong/backend/api/hospital/repository/HospitalRepository.java
@@ -1,14 +1,13 @@
 package com.ppiyong.backend.api.hospital.repository;
 
 import com.ppiyong.backend.api.hospital.entity.Hospital;
+import com.ppiyong.backend.api.member.entity.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-import java.util.List;
 import java.util.Optional;
+import java.util.List;
 
-@Repository
-public interface HospitalRepository extends JpaRepository<Hospital, String> {
-    Optional<Hospital> findByPlaceId(String placeId);
-    List<Hospital> findByPlaceIdIn(List<String> placeIds);
+public interface HospitalRepository extends JpaRepository<Hospital, Long> {
+    Optional<Hospital> findByHospitalId(Long HospitalId);
 }
+
+
