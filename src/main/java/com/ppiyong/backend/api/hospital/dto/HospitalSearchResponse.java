768aad415dfbfd39d8ad2227fca96d95d144a896
LHL <lin1137001@gmail.com>
2025-04-30 02:38:00 +0900

    feature/#5 : 병원 조회 때 즐겨찾기된 병원정보 반환

--- src/main/java/com/ppiyong/backend/api/hospital/dto/HospitalSearchResponse.java
+++ src/main/java/com/ppiyong/backend/api/hospital/dto/HospitalSearchResponse.java
@@ -5,8 +5,7 @@ import com.ppiyong.backend.api.hospital.dto.KakaoRestApi.KakaoCategorySearchResp
 import lombok.Getter;
 
 import java.util.List;
-import java.util.stream.Collectors;
-
+import java.util.Set;
 
 @Getter
 public class HospitalSearchResponse {
@@ -18,14 +17,25 @@ public class HospitalSearchResponse {
         this.hospitals = hospitals;
     }
 
-    public static HospitalSearchResponse ofFiltered(KakaoCategorySearchResponse response, List<Document> documents) {
+    // likedHospitalIds 추가
+    public static HospitalSearchResponse ofFiltered(
+            KakaoCategorySearchResponse response,
+            List<Document> documents,
+            Set<Long> likedHospitalIds // 추가
+    ) {
         HospitalSearchMeta meta = new HospitalSearchMeta(
                 response.getMeta().getEnd(),
                 response.getMeta().getPageableCount()
         );
 
+        // isLike 값을 likedHospitalIds 기반으로 설정
         List<HospitalInfo> hospitalInfos = documents.stream()
-                .map(HospitalInfo::of)
+                .map(document ->
+                        HospitalInfo.of(
+                                document,
+                                likedHospitalIds.contains(document.getId()) // isLike 계산
+                        )
+                )
                 .toList();
 
         return new HospitalSearchResponse(meta, hospitalInfos);
