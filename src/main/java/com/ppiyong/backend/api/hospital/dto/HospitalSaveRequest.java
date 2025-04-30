b022ce3e13008b7c1e0464160345b01acbce2469
LHL <lin1137001@gmail.com>
2025-04-30 19:02:03 +0900

    feature/#5 :

--- src/main/java/com/ppiyong/backend/api/hospital/dto/HospitalSaveRequest.java
+++ src/main/java/com/ppiyong/backend/api/hospital/dto/HospitalSaveRequest.java
@@ -4,12 +4,13 @@ import com.ppiyong.backend.api.hospital.domain.Department;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 
 import lombok.NoArgsConstructor;
 
 import java.math.BigDecimal;
-
+@Builder
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
