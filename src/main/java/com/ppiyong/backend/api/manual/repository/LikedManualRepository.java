cf8106e0918fe2a7a272cf4b14566a49896cdedc
LHL <lin1137001@gmail.com>
2025-04-30 20:14:48 +0900

    feature/#14 : member와 manual 연관 repository

---
+++ src/main/java/com/ppiyong/backend/api/manual/repository/LikedManualRepository.java
@@ -0,0 +1,16 @@
+package com.ppiyong.backend.api.manual.repository;
+
+import com.ppiyong.backend.api.manual.entity.LikedManual;
+import com.ppiyong.backend.api.member.entity.Member;
+import com.ppiyong.backend.api.manual.entity.Manual;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface LikedManualRepository extends JpaRepository<LikedManual, Long> {
+    Optional<LikedManual> findByMemberAndManual(Member member, Manual manual);
+    List<LikedManual> findByMember(Member member);
+    List<LikedManual> findByMemberAndIsLikeTrue(Member member);
+
+}
