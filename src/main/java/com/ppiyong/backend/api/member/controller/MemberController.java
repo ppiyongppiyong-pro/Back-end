9fc2250481d5796217bb2a697a2e64ea8130ccc8
kite_U <yurry45@naver.com>
2025-05-04 03:31:40 +0900

    feature/#12: MemberController, MemberService 생성 ( 회원정보 조회, 회원정보 수정 구현 )

---
+++ src/main/java/com/ppiyong/backend/api/member/controller/MemberController.java
@@ -0,0 +1,52 @@
+package com.ppiyong.backend.api.member.controller;
+
+import com.ppiyong.backend.api.member.dto.request.MypageRequestDto;
+import com.ppiyong.backend.api.member.dto.response.MypageResponseDto;
+import com.ppiyong.backend.api.member.service.MemberServiceImpl;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/v1/members")
+@RequiredArgsConstructor
+public class MemberController {
+
+    private final MemberServiceImpl memberService;
+
+    @Operation(summary = "회원 정보 수정하기", description = """
+            회원 정보를 수정합니다.<br>
+            헤더에 accessToken을 넣어주세요.
+            """, parameters = {@Parameter(name = "MypageRequest", description = "회원가입 요청 객체",
+            content = @Content(
+                    mediaType = "application/json",
+                    schema = @Schema(implementation = MypageRequestDto.class)
+            ))})
+    @PatchMapping("/profiles")
+    public MypageResponseDto updateProfile(
+            @RequestBody MypageRequestDto requestDto,
+            @RequestHeader("Authorization") String authToken
+    ) {
+        String token = authToken.startsWith("Bearer ") ?
+                authToken.substring(7) : authToken;
+        return memberService.addMemberInfo(token, requestDto);
+    }
+
+
+    @Operation(summary = "마이페이지 조회하기", description = """
+            마이페이지를 조회합니다.<br>
+            헤더에 accessToken을 넣어주세요.
+            """)
+    @GetMapping("/profiles")
+    public MypageResponseDto getProfile(
+            @RequestHeader("Authorization") String authToken
+    ) {
+        authToken = authToken.startsWith("Bearer ") ?
+                authToken.substring(7) : authToken;
+
+        return memberService.getMyInfo(authToken);
+    }
+}
