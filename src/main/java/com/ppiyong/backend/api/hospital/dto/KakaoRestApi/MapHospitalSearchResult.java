a90b40edb4568d8824f8d8922e0385babb971a8d
LHL <lin1137001@gmail.com>
2025-05-03 21:04:36 +0900

    feature/#5 : paginationInfo에 @JsonProperty(meta), hospitalInfoOnMaps에 @JsonProperty(documents) 명시

--- src/main/java/com/ppiyong/backend/api/hospital/dto/KakaoRestApi/MapHospitalSearchResult.java
+++ src/main/java/com/ppiyong/backend/api/hospital/dto/KakaoRestApi/MapHospitalSearchResult.java
@@ -1,5 +1,6 @@
 package com.ppiyong.backend.api.hospital.dto.KakaoRestApi;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -9,6 +10,9 @@ import java.util.List;
 @AllArgsConstructor
 @NoArgsConstructor
 public class MapHospitalSearchResult {
+    @JsonProperty("meta") //meta 꼭 쓰기
     private PaginationInfo paginationInfo;
+
+    @JsonProperty("documents") //documents 꼭 쓰기
     private List<HospitalInfoOnMap> hospitalInfoOnMaps;
 }
