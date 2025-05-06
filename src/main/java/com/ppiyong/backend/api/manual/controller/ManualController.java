bcbfdd889ce8d6312f6226dfc6b5fc09569bcad6
LHL <lin1137001@gmail.com>
2025-05-07 01:44:52 +0900

    feature/#40 : 병원 즐겨찾기 @Tag 추가

--- src/main/java/com/ppiyong/backend/api/manual/controller/ManualController.java
+++ src/main/java/com/ppiyong/backend/api/manual/controller/ManualController.java
@@ -8,6 +8,7 @@ import com.ppiyong.backend.api.manual.service.ManualService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
@@ -16,6 +17,7 @@ import java.util.List;
 @RestController
 @RequestMapping("/api/v1/manuals")
 @RequiredArgsConstructor
+@Tag(name = "Manual", description = "매뉴얼 관련 API")
 public class ManualController {
     private final ManualService manualService;
 
