2a369054d43e1eaf000ebdfe56fcce803f7e23d9
kite_U <yurry45@naver.com>
2025-05-27 00:03:50 +0900

    fix: Swagger에 Server URL 등록

--- src/main/java/com/ppiyong/backend/global/config/SwaggerConfig.java
+++ src/main/java/com/ppiyong/backend/global/config/SwaggerConfig.java
@@ -1,15 +1,22 @@
 package com.ppiyong.backend.global.config;
 
+import io.swagger.v3.oas.models.servers.Server;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import java.util.List;
+
 @Configuration
 public class SwaggerConfig {
+
+    @Value("${swagger.server.url}")
+    private String serverUrl;
 
     @Bean
     public OpenAPI openAPI() {
@@ -21,6 +28,9 @@ public class SwaggerConfig {
         final String securitySchemeName = "bearerAuth";
 
         return new OpenAPI()
+                .servers(List.of(
+                        new Server().url(serverUrl)
+                ))
                 .addSecurityItem(new SecurityRequirement().addList(securitySchemeName))
                 .components(
                         new Components().addSecuritySchemes(securitySchemeName,
